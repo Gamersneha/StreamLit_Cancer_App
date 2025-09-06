ed5fbac24590587f184d5cacf5fe8a2094a11fde
Gamersneha <snehasaha795@gmail.com>
2025-09-06 22:09:52 +0530

    Add model training and serialization functionality; implement Streamlit app structure

--- model/main.py
+++ model/main.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+import pickle
 
 def create_model(data):
     X = data.drop(['diagnosis'], axis=1)
@@ -47,6 +48,12 @@ def main():
     
     model, scaler = create_model(data)
 
+    with open('model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+
+    with open('model/scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
+    
 
 if __name__ == '__main__':
    main()
